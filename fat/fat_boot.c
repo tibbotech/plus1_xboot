3e6dc808ce2ac2f7e1520f52e74218dd3a29bdd8
vincent.shih <vincent.shih@sunplus.com>
2020-12-02 15:47:51 +0800

    [xboot] Implement the support of FAT16.

--- fat/fat_boot.c
+++ fat/fat_boot.c
@@ -14,19 +14,43 @@ static const unsigned char FILENAMES[FAT_FILES][12] =
 	"ISPBOOOTBIN","U-BOOT  IMG","UIMAGE     ","DTB        "
 };
 
-static u32 search_files(fat_info *info, u8 *buffer, u8 type);
+static u32 search_fat32_files(fat_info *info, u8 *buffer, u8 type);
+#ifdef FAT16_SUPPORT
+static u32 search_fat16_files(fat_info *info, u8 *buffer, u8 type);
+#endif
 
 static u32 next_cluster(fat_info *info, u32 currentClus, u8 *buffer)
 {
 	u32 cluster = currentClus;
-	u32 sector;
-	fatable_info *fatable;
+	u32 sector = 0;
+	fat32table_info *fat32table;
+#ifdef FAT16_SUPPORT
+	fat16table_info *fat16table;
+#endif
 
-	sector = info->fat1Sect +
-		(cluster >> (info->bytePerSectInPower - 2));
+	if (info->fatType == FAT_32) {
+		sector = info->fat1Sect +
+				(cluster >> (info->bytePerSectInPower - 2));
+	}
+#ifdef FAT16_SUPPORT
+	else if (info->fatType == FAT_16) {
+		sector = info->fat1Sect +
+				(cluster >> (info->bytePerSectInPower - 1));
+	}
+#endif
+
 	info->read_sector(sector, 1, (u32 *)buffer);
-	fatable = (fatable_info *)(buffer);
-	cluster = fatable->fat[cluster & ((info->bytePerSect >> 2) - 1)];	/* X mod 2^n == X & (2^n - 1) */
+
+	if (info->fatType == FAT_32) {
+		fat32table = (fat32table_info *)(buffer);
+		cluster = fat32table->fat[cluster & ((info->bytePerSect >> 2) - 1)];	/* X mod 2^n == X & (2^n - 1) */
+	}
+#ifdef FAT16_SUPPORT
+	else if (info->fatType == FAT_16) {
+		fat16table = (fat16table_info *)(buffer);
+		cluster = fat16table->fat[cluster & ((info->bytePerSect >> 1) - 1)];	/* X mod 2^n == X & (2^n - 1) */
+	}
+#endif
 
 	return cluster;
 }
@@ -58,7 +82,11 @@ u32 fat_read_file(u32 idx, fat_info *info, u8 *buffer, u32 offset, u32 length, u
 	/* go to target cluster */
 	cluster = info->fileInfo[idx].cluster;
 	while (off >= info->sectPerClus) {
-		if (cluster >= 0x0FFFFFF8) {
+		if (((info->fatType == FAT_32) && (cluster >= 0x0FFFFFF8))
+#ifdef FAT16_SUPPORT
+			|| ((info->fatType == FAT_16) && (cluster >= 0xFFF5))
+#endif
+			) {
 			return FAIL;
 		}
 
@@ -76,8 +104,17 @@ u32 fat_read_file(u32 idx, fat_info *info, u8 *buffer, u32 offset, u32 length, u
 			count = info->bytePerSect;
 		}
 
-		sector = info->clust0Sect +
-			((cluster - info->rootClus) * info->sectPerClus);
+		if (info->fatType == FAT_32) {
+			sector = info->clust0Sect +
+					((cluster - info->rootClus) * info->sectPerClus);
+		}
+#ifdef FAT16_SUPPORT
+		else if (info->fatType == FAT_16) {
+			sector = info->rootSectStart + info->rootSect +
+					((cluster - FAT_DATA_1ST_CLUS_NUM) * info->sectPerClus);
+		}
+#endif
+
 		info->read_sector((sector + off), 1, (u32 *)buffer);
 
 		for (j = remain; j < count; j++) {
@@ -99,8 +136,16 @@ u32 fat_read_file(u32 idx, fat_info *info, u8 *buffer, u32 offset, u32 length, u
 
 	/* deal with blocks whose size is smaller than a cluster */
 	if (off & (info->sectPerClus - 1)) { // Not on cluster
-		sector = off + info->clust0Sect +
-			((cluster - info->rootClus) * info->sectPerClus);
+		if (info->fatType == FAT_32) {
+			sector = off + info->clust0Sect +
+					((cluster - info->rootClus) * info->sectPerClus);
+		}
+#ifdef FAT16_SUPPORT
+		else if (info->fatType == FAT_16) {
+			sector = off + info->rootSectStart + info->rootSect +
+					((cluster - FAT_DATA_1ST_CLUS_NUM) * info->sectPerClus);
+		}
+#endif
 
 		// read until next cluster
 		while (off & (info->sectPerClus - 1)) {
@@ -116,7 +161,7 @@ u32 fat_read_file(u32 idx, fat_info *info, u8 *buffer, u32 offset, u32 length, u
 			dest += info->bytePerSect;
 			size -= info->bytePerSect;
 		}
-		cluster = next_cluster(info, cluster, (u8*)buffer);
+		cluster = next_cluster(info, cluster, (u8 *)buffer);
 	}
 
 #if defined(CONFIG_HAVE_USB_DISK) && defined(FAT_USB_4K_READ)
@@ -129,8 +174,16 @@ u32 fat_read_file(u32 idx, fat_info *info, u8 *buffer, u32 offset, u32 length, u
 	/* deal with blocks whose size is bigger or equal to a cluster */
 	while (size >= bytesPerClus) {
 		size -= bytesPerClus;
-		sector = info->clust0Sect +
-			((cluster - info->rootClus) * info->sectPerClus);
+		if (info->fatType == FAT_32) {
+			sector = info->clust0Sect +
+					((cluster - info->rootClus) * info->sectPerClus);
+		}
+#ifdef FAT16_SUPPORT
+		else if (info->fatType == FAT_16) {
+			sector = info->rootSectStart + info->rootSect +
+					((cluster - FAT_DATA_1ST_CLUS_NUM) * info->sectPerClus);
+		}
+#endif
 
 		for (j = 0; j < info->sectPerClus; j += txfer_sects) {
 			info->read_sector((sector + j), txfer_sects, (u32 *)dest);
@@ -140,12 +193,20 @@ u32 fat_read_file(u32 idx, fat_info *info, u8 *buffer, u32 offset, u32 length, u
 		if (size == 0) {
 			return PASS;
 		} else {
-			cluster = next_cluster(info, cluster, (u8*)buffer);
+			cluster = next_cluster(info, cluster, (u8 *)buffer);
 		}
 	}
 
-	sector = info->clust0Sect +
-		((cluster - info->rootClus) * info->sectPerClus);
+	if (info->fatType == FAT_32) {
+		sector = info->clust0Sect +
+				((cluster - info->rootClus) * info->sectPerClus);
+	}
+#ifdef FAT16_SUPPORT
+	else if (info->fatType == FAT_16) {
+		sector = info->rootSectStart + info->rootSect +
+				((cluster - FAT_DATA_1ST_CLUS_NUM) * info->sectPerClus);
+	}
+#endif
 
 	/* deal with remaining sectors which is smaller than a cluster*/
 	while (size >= info->bytePerSect) {
@@ -164,6 +225,37 @@ u32 fat_read_file(u32 idx, fat_info *info, u8 *buffer, u32 offset, u32 length, u
 	return PASS;
 }
 
+#if 0	//for debug
+static void prn_data(u8 *buffer, int length)
+{
+	int i;
+	u32 j = 0;	/* initial address */
+
+	for (i = 0; i < length; i++) {
+		if (i == 0) {
+			prn_dword0(j); prn_string(" : ");
+			prn_byte(buffer[i]);
+			j += 0x10;
+		} else if ((((i + 1) % 16) == 0)) {
+			prn_byte(buffer[i]);
+			prn_string("\n");
+
+			if (i < (length - 1)) {
+				prn_dword0(j); prn_string(" : ");
+				j += 0x10;
+			}
+		} else if (((i + 1) % 8) == 0) {
+			prn_byte(buffer[i]);
+			prn_string(" ");
+		} else
+			prn_byte(buffer[i]);
+	}
+
+	if ((length % 16) != 0)
+		prn_string("\n");
+}
+#endif
+
 /*
  * fat_boot
  * @type	USB_ISP or SDCARD_ISP
@@ -172,16 +264,21 @@ u32 fat_read_file(u32 idx, fat_info *info, u8 *buffer, u32 offset, u32 length, u
  */
 u32 fat_boot(u32 type, u32 port, fat_info *info, u8 *buffer)
 {
-	fat32_bpb *bpb;
+	fat32_bpb *bpb32;
+#ifdef FAT16_SUPPORT
+	fat16_bpb *bpb16;
+#endif
 	u32 tmp;
 	u32 ret;
-	
+	int entry = 0;
+
 	CSTAMP(0xFAB00000);
 
 	dbg_info();
 	info->startSector = 0;
 	info->bytePerSect = 0;
 	info->init = NULL;
+	info->fatType = FAT_UNKNOW;
 
 	/* Set init function and read_sector function*/
 	if (type == USB_ISP) {
@@ -230,57 +327,109 @@ u32 fat_boot(u32 type, u32 port, fat_info *info, u8 *buffer)
 	CSTAMP(0xFAB00003);
 	dbg_info();
 
+#if 0	//for debug
+	prn_string("/* MBR */\n");
+	prn_data(buffer, 512);
+#endif
+
 	/*
 	 * Check MBR
 	 */
 
-	// MBR or BPB ?
-	bpb = (fat32_bpb*)buffer;
+	// MBR
+	bpb32 = (fat32_bpb *)buffer;
+#ifdef FAT16_SUPPORT
+	bpb16 = (fat16_bpb *)buffer;
+#endif
 
+check_partition_entry:
 	// MBR criteria:
 	// 1. signature        : 55 aa
 	// 2. partition status : 80=active, 00=inactive, 00~7f=invalid
 	// 3. partition type   : non-zero
-	if (buffer[0x1fe] == 0x55 && buffer[0x1ff] == 0xaa && 
-		(buffer[0x1be] & ~0x80) == 0 && buffer[0x1c2] != 0) {
+	if (buffer[0x1fe] == 0x55 && buffer[0x1ff] == 0xaa &&
+		(buffer[0x1be + entry * FAT_PART_ENTRY_SIZE] & ~0x80) == 0 &&
+		buffer[0x1c2 + entry * FAT_PART_ENTRY_SIZE] != 0 &&
+		buffer[0x1c2 + entry * FAT_PART_ENTRY_SIZE] != 0xF) {
 		/* get start sector */
-		info->startSector = buffer[457] << 24 |
-			buffer[456] << 16 |
-			buffer[455] << 8  |
-			buffer[454];
+		info->startSector = buffer[457 + entry * FAT_PART_ENTRY_SIZE] << 24 |
+			buffer[456 + entry * FAT_PART_ENTRY_SIZE] << 16 |
+			buffer[455 + entry * FAT_PART_ENTRY_SIZE] << 8  |
+			buffer[454 + entry * FAT_PART_ENTRY_SIZE];
 
-		prn_string("part@"); prn_dword(info->startSector);
+		prn_string("part"); prn_byte((u8)entry); prn_string("@");
+		prn_dword(info->startSector);
 
 		/* get BPB */
 		info->read_sector(info->startSector, 1, (u32 *)buffer);
 	} else {
-		prn_string("no mbr\n");
+		if (buffer[0x1fe] != 0x55 || buffer[0x1ff] != 0xaa ||
+			(buffer[0x1be + entry * FAT_PART_ENTRY_SIZE] & ~0x80) != 0)
+			prn_string("no MBR\n");
+		else if (buffer[0x1c2 + entry * FAT_PART_ENTRY_SIZE] == 0)
+			prn_string("no PARTITION\n");
+		else if (buffer[0x1c2 + entry * FAT_PART_ENTRY_SIZE] == 0xF)
+			prn_string("extended PARTITION (not supported)\n");
+
+		return FAIL;
 	}
 
 	dbg_info();
 
+#if 0	//for debug
+	prn_string("/* DBR */\n");
+	prn_data(buffer, 512);
+#endif
+
 	/*
 	 * check FAT32
 	 */
-	if (!((((bpb->systemType[1] << 16) | bpb->systemType[0]) == FAT32_L) &&
-				(((bpb->systemType[3] << 16) | bpb->systemType[2]) == FAT32_U))) {
-		prn_string("Not fat32\n");
+	if ((((bpb32->systemType[1] << 16) | bpb32->systemType[0]) == FAT32_L) &&
+				(((bpb32->systemType[3] << 16) | bpb32->systemType[2]) == FAT32_U)) {
+		info->fatType = FAT_32;
+
+		/* Get infomation of BPB */
+		info->bytePerSect = (bpb32->bytePerSect[1] << 8) + bpb32->bytePerSect[0];
+		info->sectPerClus = bpb32->sectPerClus;
+		info->numOfReserveSect = bpb32->sectResearve;
+		info->numFatCopy = bpb32->numFat;
+		info->sectPerFat =bpb32->sectPerFat;
+		info->rootClus = bpb32->rootClusNum;
+
+		info->fat1Sect = info->startSector + info->numOfReserveSect;
+		info->clust0Sect = info->fat1Sect + (info->sectPerFat * info->numFatCopy);
+
+		prn_string("FAT32 file system\n");
+	}
+#ifdef FAT16_SUPPORT
+	else if ((((bpb16->systemType[1] << 16) | bpb16->systemType[0]) == FAT16_L) &&
+				(((bpb16->systemType[3] << 16) | bpb16->systemType[2]) == FAT16_U)) {
+		info->fatType = FAT_16;
+
+		/* Get infomation of BPB */
+		info->bytePerSect = ((u16)bpb16->bytePerSect[1] << 8) + bpb16->bytePerSect[0];
+		info->sectPerClus = bpb16->sectPerClus;
+		info->numOfReserveSect = bpb16->sectResearve;
+		info->numFatCopy = bpb16->numFat;
+		info->sectPerFat = ((u32)bpb16->fatSz16[1] << 8) + bpb16->fatSz16[0];
+		info->rootEntCnt = ((u16)bpb16->rootEntCnt[1] << 8) + bpb16->rootEntCnt[0];
+		info->rootSectStart = info->startSector + info->numOfReserveSect + (info->sectPerFat * info->numFatCopy);
+		info->rootSect = ((u32)info->rootEntCnt * 32 + (u32)info->bytePerSect - 1) / (u32)info->bytePerSect;
+		info->fat1Sect = info->startSector + info->numOfReserveSect;
+
+		prn_string("FAT16 file system\n");
+	} else {
+		prn_string("Not FAT32/FAT16\n");
+		return FAIL;
+		/* Not FAT32/FAT16...*/
+	}
+#else
+	else {
+		prn_string("Not FAT32\n");
 		return FAIL;
 		/* Not FAT32...*/
 	}
-
-	/*
-	 * Get infomation of BPB
-	 */
-	info->bytePerSect = (bpb->bytePerSect[1] << 8) + bpb->bytePerSect[0];
-	info->sectPerClus = bpb->sectPerClus;
-	info->numOfReserveSect = bpb->sectResearve;
-	info->numFatCopy = bpb->numFat;
-	info->sectPerFat =bpb->sectPerFat;
-	info->rootClus = bpb->rootClusNum;
-
-	info->fat1Sect = info->startSector + info->numOfReserveSect;
-	info->clust0Sect = info->fat1Sect + (info->sectPerFat * info->numFatCopy);
+#endif
 
 	/* Because of lack of mod and divide operations */
 	tmp = info->bytePerSect;
@@ -294,15 +443,36 @@ u32 fat_boot(u32 type, u32 port, fat_info *info, u8 *buffer)
 	 * Search files
 	 */
 	dbg_info();
-	ret = search_files(info, (u8*)buffer, type);
-	if ( ret == FAIL) {
-		dbg_info();
-		return FAIL;
+	if (info->fatType == FAT_32)
+		ret = search_fat32_files(info, (u8*)buffer, type);
+#ifdef FAT16_SUPPORT
+	else if (info->fatType == FAT_16)
+		ret = search_fat16_files(info, (u8*)buffer, type);
+#endif
+
+	if (ret == FAIL) {
+		info->startSector = 0;
+		info->bytePerSect = 0;
+		info->fatType = FAT_UNKNOW;
+		entry++;
+
+		if (entry >= FAT_MAX_PARTITION) {
+			dbg_info();
+			return FAIL;
+		}
+
+		ret = info->read_sector(info->startSector, 1, (u32 *)buffer);
+		if (ret != ROM_SUCCESS) {
+			dbg();
+			return FAIL;
+		}
+
+		goto check_partition_entry;
 	}
 	return ret;
 }
 
-static u32 search_files(fat_info *info, u8 *buffer, u8 type)
+static u32 search_fat32_files(fat_info *info, u8 *buffer, u8 type)
 {
 	u32 i;
 	u32 count;
@@ -311,8 +481,8 @@ static u32 search_files(fat_info *info, u8 *buffer, u8 type)
 	u32 fdbOffset;
 	u32 currentSect;
 	fdb_info *fdb;
-	u32 filecount = (type == SDCARD_ISP)?FAT_FILES:1;
-	
+	u32 filecount = (type == SDCARD_ISP) ? FAT_FILES : 1;
+
 	dbg_info();
 	count = 0;
 	nextClus = info->rootClus;
@@ -335,15 +505,14 @@ static u32 search_files(fat_info *info, u8 *buffer, u8 type)
 						info->fileInfo[i].sectPos = info->clust0Sect +
 							((info->fileInfo[i].cluster - info->rootClus) * info->sectPerClus);
 						count++;
-						if(type == USB_ISP && info->fileInfo[0].size !=0)
-						{
+						if(type == USB_ISP && info->fileInfo[0].size != 0) {
 							dbg_info();
 							return PASS;
 						}
-						/* sdcard boot,check ispbooot.bin u-boot uimage  dtb  */
+						/* sdcard boot,check ispbooot.bin u-boot uimage dtb */
 						if (type == SDCARD_ISP && count == filecount) {
 							dbg_info();
-							/* 
+							/*
 							// only one return type
 							if (fat_type == CARD0_ISP || fat_type == CARD1_ISP ||
 								fat_type == CARD2_ISP || fat_type == CARD3_ISP)
@@ -360,7 +529,7 @@ static u32 search_files(fat_info *info, u8 *buffer, u8 type)
 		/* next cluster */
 		nextClus = next_cluster(info, nextClus, (u8*)buffer);
 	}
-	
+
 	/* sdcard isp ,check ispbooot.bin. sdcard boot,check ispbooot.bin u-boot uimage dtb  */
 	if(type == SDCARD_ISP && info->fileInfo[0].size !=0)
 	{
@@ -371,6 +540,61 @@ static u32 search_files(fat_info *info, u8 *buffer, u8 type)
 	/* can't find files that we need.... */
 	return FAIL;
 }
+
+#ifdef FAT16_SUPPORT
+static u32 search_fat16_files(fat_info *info, u8 *buffer, u8 type)
+{
+	u32 i;
+	u32 count;
+	u32 sectOffset;
+	u32 fdbOffset;
+	fdb_info *fdb;
+	u32 filecount = (type == SDCARD_ISP) ? FAT_FILES : 1;
+
+	dbg_info();
+	count = 0;
+	sectOffset = 0;
+
+	while (sectOffset < info->rootSect) {
+		dbg_info();
+		fdbOffset = 0;
+		info->read_sector(info->rootSectStart + sectOffset, 1, (u32 *)buffer);
+		while (fdbOffset < info->bytePerSect) {
+			fdb = (fdb_info *)(&buffer[fdbOffset]);
+			for (i = 0; i < filecount; i++) {
+				if (memcmp((u8 *)fdb->name, (u8 *)FILENAMES[i], FAT_FILENAMELEN) == 0) {
+					info->fileInfo[i].size = fdb->fileSize;
+					info->fileInfo[i].cluster = fdb->clusterL;
+					info->fileInfo[i].sectPos = info->rootSectStart + info->rootSect +
+									(info->fileInfo[i].cluster - FAT_DATA_1ST_CLUS_NUM) *
+										info->sectPerClus;
+					count++;
+					if(type == USB_ISP && info->fileInfo[0].size != 0) {
+						dbg_info();
+						return PASS;
+					}
+					/* sdcard boot, check ispbooot.bin u-boot uimage dtb */
+					if (type == SDCARD_ISP && count == filecount) {
+						dbg_info();
+						/*
+						// only one return type
+						if (fat_type == CARD0_ISP || fat_type == CARD1_ISP ||
+							fat_type == CARD2_ISP || fat_type == CARD3_ISP)
+							return PASS2;
+						*/
+						return PASS;
+					}
+				}
+			}
+			fdbOffset += FAT_FDB_SIZE;
+		}
+		sectOffset++;
+	}
+	dbg_info();
+	/* can't find files that we need.... */
+	return FAIL;
+}
+#endif
 
 /*sdcard do isp or boot is decide by ISPBOOOT.BIN size
   do isp: ISPBOOOT.BIN size > xboot.img size (64k);
--- include/fat/fat.h
+++ include/fat/fat.h
@@ -4,12 +4,32 @@
 #include <types.h>
 //#include <nand_boot/nandop.h>
 
+//#define FAT16_SUPPORT
+
+#define FAT_UNKNOW		0
+#define FAT_32			1
+#ifdef FAT16_SUPPORT
+#define FAT_16			2
+#endif
+
 //#define MAX_SECTOR_SIZE     1024
 #define FAT32_U 		0x20202032
 #define FAT32_L 		0x33544146
+#ifdef FAT16_SUPPORT
+#define FAT16_U 		0x20202036
+#define FAT16_L 		0x31544146
+#endif
 #define FAT_FDB_SIZE 		32
+#define FAT_PART_ENTRY_SIZE	0x10
+#define FAT_MAX_PARTITION	4
+#ifdef FAT16_SUPPORT
+#define FAT_DATA_1ST_CLUS_NUM	2
+#endif
 #define FAT_FILES 		4
-#define FAT_MAXSECTSIZE 	(16 * 1024)
+#define FAT32_MAXSECTSIZE 	(16 * 1024)
+#ifdef FAT16_SUPPORT
+#define FAT16_MAXSECTSIZE 	(32 * 1024)
+#endif
 #define FAT_FILENAMELEN 	11
 //#define SECTORSIZE 		512
 
@@ -28,8 +48,16 @@ typedef struct {
 	u32 rootClus;
 	u32 fat1Sect;
 	u32 clust0Sect;
+#ifdef FAT16_SUPPORT
+	u32 rootSectStart;
+	u32 rootSect;
+#endif
 	u16 bytePerSect;
 	u16 numOfReserveSect;
+#ifdef FAT16_SUPPORT
+	u16 rootEntCnt;
+#endif
+	u8 fatType;
 	u8 sectPerClus;
 	u8 numFatCopy;
 	u8 bytePerSectInPower;
@@ -54,6 +82,21 @@ typedef struct {
 	u16 systemType[4];
 } fat32_bpb;
 
+#ifdef FAT16_SUPPORT
+typedef struct {
+	u8 info1[11];
+	u8 bytePerSect[2];
+	u8 sectPerClus;
+	u16 sectResearve;
+	u8 numFat;
+	u8 rootEntCnt[2];
+	u8 info2[3];
+	u8 fatSz16[2];
+	u8 info3[30];
+	u16 systemType[4];
+} fat16_bpb;
+#endif
+
 typedef struct {
 	u8 name[11];	/* short file name only */
 	u8 reserved[9];
@@ -65,8 +108,14 @@ typedef struct {
 } fdb_info;
 
 typedef struct {
-	u32 fat[FAT_MAXSECTSIZE];
-} fatable_info;
+	u32 fat[FAT32_MAXSECTSIZE];
+} fat32table_info;
+
+#ifdef FAT16_SUPPORT
+typedef struct {
+	u16 fat[FAT16_MAXSECTSIZE];
+} fat16table_info;
+#endif
 
 //u32 search_files(u32 files, fat_info *info, u8 *buffer, u8 fat_type);
 u32 fat_boot(u32 type, u32 port, fat_info *info, u8 *buffer);
