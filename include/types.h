2e939ceb2fd370ba2d9cc24acddf8465c67cfdf8
Dvorkin Dmitry <dvorkin@tibbo.com>
2021-01-06 15:01:05 +0300

    Fix compiler error: Wells, size_t is always defined in stddef.h!

--- include/types.h
+++ include/types.h
@@ -4,8 +4,6 @@
 //#include <stddef.h>
 #include <stdint.h>
 
-typedef unsigned long       size_t;
-
 typedef unsigned long long  u64;
 typedef unsigned int        u32;
 typedef unsigned short      u16;
