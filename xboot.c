437702714265dcab3f91061850202e93cd728d75
Akira.Yu <akira.yu@sunplus>
2023-07-07 20:12:09 +0800

    [HW init] Set GMAC TXC and RXC softpad value.

--- xboot.c
+++ xboot.c
@@ -548,8 +548,8 @@ static void init_hw(void)
 	*(volatile u32 *)(ARM_TSGEN_WR_BASE + 0x08) = 0; // CNTCV[31:0]
 	*(volatile u32 *)(ARM_TSGEN_WR_BASE + 0x0C) = 0; // CNTCV[63:32]
 
-	//PAD_CTL2_REG->cfg[30] = 0x00000000; // GMAC Softpad control register 0 : bit31-->0 ,TXC non-GPIO mode
-	//PAD_CTL2_REG->cfg[31] = 0x00000000; // GMAC Softpad control register 1 : bit31-->0 ,RXC non-GPIO mode
+	PAD_CTL2_REG->cfg[30] = 0x00044800; // GMAC Softpad control register 0 : bit31-->0 ,TXC non-GPIO mode
+	PAD_CTL2_REG->cfg[31] = 0x80000000; // GMAC Softpad control register 1 : bit31-->1 ,RXC GPIO mode
 	MOON3_REG_AO->clkgen[0] = RF_MASK_V_CLR(0x1000); //G3.23 bit12 GMAC_PHYSEL-->0 ,set GMAC to use RGMII PHY Interface. 
 
 	// Turn on power of NPU (NPU_PWR_EN, GPIO65).
