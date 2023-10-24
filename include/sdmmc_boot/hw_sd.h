512282fcc0f290018862939f2b07bdaee1d04e7e
Li-hao Kuo <lhjeff911@gmail.com>
2023-10-24 21:01:35 +0800

    [SD] restore setting  .

--- include/sdmmc_boot/hw_sd.h
+++ include/sdmmc_boot/hw_sd.h
@@ -1,6 +1,10 @@
 #include "config.h"
 #include "common.h"
 
-#ifdef PLATFORM_SP7350
-#include "hw_sd_sp7350.h"
+#ifdef PLATFORM_I143
+#include "hw_sd_143.h"
+#elif defined(PLATFORM_Q645) || defined(PLATFORM_SP7350)
+#include "hw_sd_645.h"
+#else
+#include "hw_sd_628.h"
 #endif
